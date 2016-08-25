e0e93dc27f9382e8a9d8ae9be1a2f1502d7be167
*buriihenry* <*buriihenry@gmail.com*>
2016-08-24 17:14:02 -0700

    Added Stuff
    These are the stuff I added

---
+++ index.php
@@ -0,0 +1,207 @@
+<!DOCTYPE HTML>
+<!--
+the mixing of colors is awesome thanks to hyperspace
+-->
+<html>
+	<head>
+		<title>BBIT CLASS 2017</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Who we are</a></li>
+							<li><a href="#two">What we do</a></li>
+							<li><a href="#three">Get in touch</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>#BBITCLASS2017</h1>
+							<p>we are happy to be the first class in the history of MUST to introduce a new site developed by <a href="https://twitter.com/hburei">burii</a><br />
+							and we out there as start-ups to change the world coz <a href="https://twitter.com/hburei"> We love tech!!</a>.</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/about.png" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>About us </h2>
+									<p>We code, we build and construct.Coffee is what keeps us going</p>
+									<ul class="actions">
+										<li><a href="#" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/enter.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Mentorship</h2>
+									<p>We have gurus in our class that are proficent in web develpment, android, business model and they help the jonior programmers to become great senior programmers.</p>
+									<ul class="actions">
+										<li><a href="#" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Note</h2>
+									<p>Surely, we need a group photo so that i can add more pics to this site. This will be work of Bruno Kigale!</p>
+									<ul class="actions">
+										<li><a href="#" class="button">Learn more</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>What we do</h2>
+							<p>BBIT class has all jacks of trades. Dont be scared when you meet one. Some like tech, others like business and others both</p>
+							<div class="features">
+								<section>
+									<span class="icon major fa-code"></span>
+									<h3>Web Development</h3>
+									<p>We have a quite good number of devs in our class. both front-end good in (html5,ccs3,js) and back-end good in php,python,wordpress,ruby,node js,c# and java .</p>
+								</section>
+								<section>  
+									<span class="icon major fa-lock"></span>
+									<h3>Security</h3>
+									<p>This class has a few individuals that have skills to cyber security by offering awesome counter measures to the new websites.</p>
+								</section>
+								<section>
+									<span class="icon major fa-cog"></span>
+									<h3>Network</h3>
+									<p>This class has large number of students that are aware with network design and troubleshooting. Hire these guys as they do good stuff.</p>
+								</section>
+								<section>
+									<span class="icon major fa-desktop"></span>
+									<h3>Desktop Applications</h3>
+									<p>We have few individuals that are into developing desktop applications using java. Hire them as they are effective in what they do.</p>
+								</section>
+								<section>
+									<span class="icon major fa-chain"></span>
+									<h3>Business Oriented</h3>
+									<p>This category majority of the students. They have good business and supply chain skills, well informed and deliver the products in time.you don't wanna mess with this group.</p>
+								</section>
+								<section>
+									<span class="icon major fa-diamond"></span>
+									<h3>Freelancers</h3>
+									<p>Like 99% of the class are freelancers.They fall in any of the above categories. They can work any where. Most of them work online, conduct business activites and are on social media.</p>
+								</section>
+							</div>
+							<ul class="actions">
+								<li><a href="#" class="button">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Get in touch</h2>
+							<p>you can send us a message or leave a comment in the form below. we will try get back to you within 24 hours .</p>
+							<div class="split style1">
+								<section>
+									<form method="post" action="#">
+										<div class="field half first">
+											<label for="name">Name</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="5"></textarea>
+										</div>
+										<ul class="actions">
+											<li><a href="" class="button submit">Send Message</a></li>
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<li>
+											<h3>Address</h3>
+											<span>Meru <br />
+											Meru-Maua Road, TN<br />
+											Kenya</span>
+										</li>
+										<li>
+											<h3>Email</h3>
+											<a href="businesstech2013@gmail.com">businesstech2013@gmail.com</a>
+										</li>
+										<li>
+											<h3>Phone</h3>
+											<span>+254706118079,+254707830071</span>
+										</li>
+										<li>
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://twitter.com/@hburei" class="fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="https://facebook.com" class="fa-facebook"><span class="label">Facebook</span></a></li>
+												<li><a href="https://github.com/buriihenry" class="fa-github"><span class="label">GitHub</span></a></li>
+												<li><a href="https://instagram.com" class="fa-instagram"><span class="label">Instagram</span></a></li>
+												<li><a href="https://linkedin.com" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; bbitclass2017. All rights reserved.</li><li>Credits: <a href="https://twitter.com/hburei">bbit and school of site</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
